fe5a6c099a2aded106c5328ddd87686a2e7f807c
emre.sekerci <emre.sekerci@HASEKI-1575>
2024-07-19 15:55:31 +0300

    Add project files.

---
+++ Pratik - İlk Uygulamam/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.WriteLine( "Emre Şekerci" );
+Console.WriteLine( "Patika'nin uzman kadrosu ve tecrübeli eğitmenleri sayesinde yazilim" +
+    "dünyasina hizli bir sekilde giris yapabilecegim için bu bootcampteyim!" );
